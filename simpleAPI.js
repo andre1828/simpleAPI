a60e8ec167e244dbf4ac8b5c0a03acf41867ee51
andre1828 <andremelo13@gmail.com>
2016-10-27 23:01:45 -0300

    changes listening port to support heroku

--- simpleAPI.js
+++ simpleAPI.js
@@ -8,6 +8,6 @@ app.use(bodyParser.urlencoded( { extended : true} ));
 
 var routes = require("./routes.js")(app);
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
       console.log('listening on port : ', server.address().port);
 });
